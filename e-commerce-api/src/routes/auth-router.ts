58b5b8345a3f5a471c5fe5678f67b543cbdc22d5
riorichard98 <riorichard12@gmail.com>
2025-01-17 17:07:39 +0700

    enhance: auth handlers in auth routes using handler wrapper

--- e-commerce-api/src/routes/auth-router.ts
+++ e-commerce-api/src/routes/auth-router.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 
 import { authHandler } from "../handler/auth-handler";
-import { catchWrapper as c } from "../middleware/catch-wrapper";
+import { handlerWrapper as h } from "../middleware/handler-wrapper";
 
 const authRouter = Router();
 
-authRouter.post('/register', c(authHandler.register)); // register
-authRouter.post('/login', c(authHandler.login)); // login
+authRouter.post('/register', h(authHandler.register)); // register
+authRouter.post('/login', h(authHandler.login)); // login
 
 export default authRouter;
