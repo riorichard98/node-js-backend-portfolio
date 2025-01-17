cdf266b87a8cb840a5a95935bf06421473ef180c
riorichard98 <riorichard12@gmail.com>
2025-01-17 12:16:59 +0700

    feat: error handler middleware

---
+++ e-commerce-api/src/middleware/error-handler.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { CustomError, ErrorResponse } from "./interface";
+
+export const errorHandler = (err: CustomError | Error, req: Request, res: Response, next: NextFunction) => {
+  const errorCode = (err as CustomError).statusCode || 500;
+  const message = (err as CustomError).message || 'Internal Server Error';
+
+  // Log full error details for debugging in 500 errors
+  if (errorCode === 500) {
+    console.error('Internal Server Error:', {
+      message: (err as Error).message,
+      stack: (err as Error).stack,
+      additionalDetails: err, // Log any additional error properties
+    });
+  }
+
+  const responseJson: ErrorResponse = { errorCode, message };
+  res.status(errorCode).json(responseJson);
+};
+
+export const throwRequestError = (message: string): void => {
+  throw ({
+    statusCode: 400,
+    message
+  })
+}
