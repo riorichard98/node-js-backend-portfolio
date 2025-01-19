89516a073e00c5a8f041562228c89c648213cf8c
riorichard98 <riorichard12@gmail.com>
2025-01-19 19:18:32 +0700

    feat: user detail & update in user handler

---
+++ e-commerce-api/src/handler/user-handler.ts
@@ -0,0 +1,29 @@
+import { Request } from "express";
+import { User } from "@prisma/client";
+import dayjs from "dayjs";
+
+import { validateRequest } from "../utils/common"
+import { updateUserSchema } from "../joi/schema/user-joi"
+import { userUsecase } from "../usecase/user-usecase"
+
+const userDetail = (req: Request) => {
+    const { fullName, email, createdAt, updatedAt } = req.user as User
+    const data = {
+        fullName,
+        email,
+        joinedAt: dayjs(createdAt).toString(),
+        updatedAt: dayjs(updatedAt).toString()
+    };
+    return { data, statusCode: 200 }
+}
+
+const updateUser = async (req: Request) => {
+    const body = validateRequest(updateUserSchema, req.body)
+    await userUsecase.updateUser(body, req.user?.userId as string)
+    return { data: {}, statusCode: 200 }
+}
+
+export const userHandler = {
+    userDetail,
+    updateUser
+}
