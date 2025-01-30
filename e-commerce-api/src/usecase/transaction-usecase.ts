1425cbac4727d679717f0b0aa844314f12e26a61
riorichard98 <riorichard12@gmail.com>
2025-01-30 10:32:22 +0700

    feat: processing payment usecase

--- e-commerce-api/src/usecase/transaction-usecase.ts
+++ e-commerce-api/src/usecase/transaction-usecase.ts
@@ -1,10 +1,13 @@
-import { Product, Transaction } from "@prisma/client";
+import { Payment, Product, Transaction } from "@prisma/client";
 import { GENERAL_ERROR_MESSAGE } from "../constants/general-error-message";
 import { BuyProductData, CreateNewPaymentData } from "../joi/interface"
 import { throwRequestError } from "../middleware/error-handler";
 import prisma from "../models/primsa-client"
 import { BuyProductResponse, CreateNewPaymentResponse } from "./interface"
 import { TRANSACTION_ERROR_MESSAGE } from "../constants/transaction-error-message";
+import { rabbitMq } from "../models/mq-client";
+import { MQ_EXCHANGE } from "../constants/mq-exchange";
+import { MQ_SEVERITY } from "../constants/mq-severity";
 
 const buyProduct = async (data: BuyProductData, userId: string): Promise<BuyProductResponse> => {
     const { productId, quantity } = data;
@@ -43,7 +46,25 @@ const makePayment = async (data: CreateNewPaymentData, userId: string): Promise<
     return { paymentId: paymentCreated.paymentId }
 }
 
+const processPayment = async (paymentId: string): Promise<void> => {
+    const paymentFound = await prisma.payment.findUnique({
+        where: {
+            paymentId,
+            status: 'PENDING'
+        }
+    }) as Payment;
+    if (!paymentFound) throwRequestError(GENERAL_ERROR_MESSAGE.DATA_NOT_EXISTS);
+    await prisma.payment.update({
+        where: { paymentId },
+        data: {
+            status: 'PROCESSING'
+        }
+    })
+    await rabbitMq.publish(MQ_EXCHANGE.PAYMENT, MQ_SEVERITY.PROCESS_PAYMENT, paymentId)
+}
+
 export const transactionUsecase = {
     buyProduct,
-    makePayment
+    makePayment,
+    processPayment
 }
